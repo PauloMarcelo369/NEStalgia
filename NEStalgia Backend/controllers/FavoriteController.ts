0b8cb733189f7c61f552380d3a1e3d61cccf6db1
Paulo Marcelo <marcelomp97447@gmail.com>
2025-03-07 09:43:00 -0300

    adicionando controller de favoritos

---
+++ NEStalgia Backend/controllers/FavoriteController.ts
@@ -0,0 +1,41 @@
+import { FavoriteService } from "../services/FavoriteService";
+import { Request, Response } from "express";
+
+interface CustomRequest extends Request {
+  user?: {
+    id: string;
+    role: string;
+  };
+}
+
+export const insertFavorite = async (req: CustomRequest, res: Response) => {
+  const gameId = parseInt(req.params.gameId, 10);
+  const userId = parseInt(req.params.id, 10);
+  try {
+    await FavoriteService.addFavorite({ userId, gameId });
+    res.status(201).json({ message: "favorito adicionado com sucesso!" });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const getAllFavorites = async (req: CustomRequest, res: Response) => {
+  const userid = parseInt(req.params.id, 10);
+  try {
+    const favoritedGames = await FavoriteService.getAllFavorites(userid);
+    res.status(200).json(favoritedGames);
+  } catch (error: any) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+export const deleteFavorite = async (req: CustomRequest, res: Response) => {
+  const gameId = parseInt(req.params.gameId, 10);
+  const userId = parseInt(req.params.id, 10);
+  try {
+    await FavoriteService.deleteFavorite({ userId, gameId });
+    res.status(200).json({ message: "o favorito foi removido!" });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
+};
